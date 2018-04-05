a16c664ae1283c1e0358174b0b8ad15198a8f395
huynguyen <huynt1608@gmail.com>
2018-04-05 12:58:18 +0700

    Remove unused import

--- src/main/java/model/APIs/JSON_APIs/ReadTempJSONFile.java
+++ src/main/java/model/APIs/JSON_APIs/ReadTempJSONFile.java
@@ -15,7 +15,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import model.APIs.JSON_APIs.DAO.UserData;
 
